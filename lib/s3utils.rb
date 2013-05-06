3c5b10161c814101c4fdcaf999bcd6b265e06a85
Ben Mishkin <bmishkin@mac.com>
2013-05-06 15:14:36 -0400

    converting to aws-sdk from old aws-s3

--- lib/s3utils.rb
+++ lib/s3utils.rb
@@ -11,38 +11,44 @@ class S3Utils
     self
   end
 
-  def copy(from, to)
-    AWS::S3::S3Object.copy(File.basename(from), File.basename(to), @bucket)
-  end
+  # def copy(from, to)
+  #   AWS::S3::S3Object.copy(File.basename(from), File.basename(to), @bucket)
+  # end
 
   def store(file_path)
-    AWS::S3::S3Object.store(File.basename(file_path), open(file_path), @bucket)
+    obj = @s3_bucket.objects[File.basename(file_path)]
+    obj.write(file_path)
+    # AWS::S3::S3Object.store(File.basename(file_path), open(file_path), @bucket)
   end
 
   def delete(file_path)
-    AWS::S3::S3Object.delete(File.basename(file_path), @bucket)
+    obj = @s3_bucket.objects[File.basename(file_path)]
+    obj.delete
+    # AWS::S3::S3Object.delete(File.basename(file_path), @bucket)
   end
 
-  def list
-    AWS::S3::Bucket.find(@bucket).objects.each do |obj|
-      puts "#{obj.bucket.name}/#{obj.key}"
-    end
-  end
+  # def list
+  #   AWS::S3::Bucket.find(@bucket).objects.each do |obj|
+  #     puts "#{obj.bucket.name}/#{obj.key}"
+  #   end
+  # end
 
   protected
 
   def connect(access_key_id, secret_access_key)
-    AWS::S3::Base.establish_connection!(
+    # AWS::S3::Base.establish_connection!(
+    @s3 = AWS::S3.new(
       :access_key_id     => access_key_id,
       :secret_access_key => secret_access_key
     )
   end
 
   def ensure_bucket_exists
-    AWS::S3::Bucket.find(@bucket)
-
-  rescue AWS::S3::NoSuchBucket
-    AWS::S3::Bucket.create(@bucket)
+    #AWS::S3::Bucket.find(@bucket)
+    @s3_bucket = @s3.buckets[@bucket]
+    rescue AWS::S3::NoSuchBucket
+    #AWS::S3::Bucket.create(@bucket)
+    @s3_the_bucket = @s3.buckets.create[@bucket]
   end
 
 end
